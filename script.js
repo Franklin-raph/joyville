f9a2fbd0645ef77a7714752101436112e4ecb08c
Franklin-raph <igboekwulusifranklin@gmail.com>
2022-12-25 16:00:29 +0100

    added the booking modal

---
+++ script.js
@@ -0,0 +1,10 @@
+let x = 8
+let y = 4
+let z = 5
+if(x>y && x>z){
+    console.log("X is greater than both y and z")
+}else if(y>z && y>x){
+    console.log("Y is greater than both x and z")
+}else{
+    console.log("Z is greater than both x and y")
+}
