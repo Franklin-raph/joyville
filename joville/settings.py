fed2b001e660a70a17e71dbd2d26d68bfea6b91b
Nwafor6 <nwaforglory6@gmail.com>
2023-01-05 02:50:26 +0000

    eidited settings.py

--- joville/settings.py
+++ joville/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['joyville.pythonanywhere.com']
 # CKEditor Settings
 CKEDITOR_UPLOAD_PATH = 'uploads/'
 CKEDITOR_IMAGE_BACKEND = "pillow"
-CKEDITOR_JQUERY_URL = '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js' 
+CKEDITOR_JQUERY_URL = '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'
 
 CKEDITOR_CONFIGS = {
     'default':
